57704734ed23bac7499544be0c062722ed92e4fb
safasefo <52189377+safasefo@users.noreply.github.com>
2019-06-25 07:03:24 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "\";
+client.on('message', message => {
+    if (message.content === '\sm') {
+        let helpEmbed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setTitle('**Server memebers**')
+        .addField("Total Members", message.guild.memberCount)
+      message.channel.send(helpEmbed);
+    }
+});
+client.login(process.env.BOT_TOKEN);
